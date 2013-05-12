701c88e14eb2eee3756c728b3b0538e019efbef6
kyryljan-serhij <kirilyan.sergei@gmail.com>
2013-05-12 18:55:53 +0300

    haida: update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,25 +1,22 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/haida/BoardConfigVendor.mk
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/haida/config
-
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := nAa_haida_defconfig
-TARGET_USES_OLD_TOOLCHAIN := true
-#TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-linaro-4.7
-#TARGET_PREBUILT_KERNEL := device/semc/haida/prebuilt/kernel
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
+# Recovery
 TARGET_RECOVERY_INITRC := device/semc/haida/recovery/init.rc
 
-#Sensor
-SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
+# Sensors
 SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
+SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9700 := yes
 
 TARGET_QCOM_HDMI_OUT := false
 
-#Recovery
-TARGET_RECOVERY_INITRC := device/semc/haida/recovery/init.rc
-
 TARGET_OTA_ASSERT_DEVICE := MT11i,MT11a,haida
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/haida/bluetooth
